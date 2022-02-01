cd5214cbb17ed5743bb8b9720abcffb849588653
ZephirSD <siegfrieddallery@gmail.com>
2022-02-01 17:38:04 +0400

    Affichage de l'alerte avec récupération de pseudo & score

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+TextEditingController _pseudo = TextEditingController();
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -28,10 +32,75 @@ class _DiceChangeState extends State<DiceChange> {
   int random1 = Random().nextInt(6) + 1;
   int random2 = Random().nextInt(6) + 1;
   void DiceChange1() {
+    int total = random1 + random2;
     setState(() {
       random1 = Random().nextInt(6) + 1;
       random2 = Random().nextInt(6) + 1;
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            scrollable: true,
+            //title: Text('Login'),
+            content: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _pseudo,
+                      decoration: InputDecoration(
+                        labelText: 'Entrez votre pseudo',
+                        icon: Icon(Icons.account_box),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20),
+                      child: Text("Votre score est de $total"),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            actions: [
+              TextButton(
+                  child: Text(
+                    "Envoyer",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      Colors.green,
+                    ),
+                  ),
+                  onPressed: () {
+                    sendScore();
+                    Navigator.pop(context);
+                  })
+            ],
+          );
+        },
+      );
     });
+  }
+
+  sendScore() async {
+    int total = random1 + random2;
+    var headers = {
+      'Content-Type': 'application/json',
+    };
+    var request = http.Request(
+        'POST', Uri.parse('http://localhost/api1/public/api/dedes'));
+    request.body = json.encode({"pseudo": _pseudo.text, "score": total});
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    } else {
+      print(response.reasonPhrase);
+    }
   }
 
   @override
